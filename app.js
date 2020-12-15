16b5c088a99d3ff63e4779dfbdabf56936756e63
Rupesh Gupta <rupeshgupta369@gmail.com>
2020-12-16 03:47:22 +0530

    html css js file completed

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("my name is rupesh gupta");
+
+var btnTranslate = document.querySelector("#btn-translate")
+
+var btnReset = document.querySelector("#btn-reset")
+
+var input = document.querySelector("#input-text");
+
+var output = document.querySelector("#output-text")
+
+
+output.value = "";
+
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+// https: //api.funtranslations.com/translate/dothraki.json
+
+// https: //api.funtranslations.com/translate/valyrian.json
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function clickHandler() {
+    var inputText = input.value;
+
+    fetch(getTranslationURL(inputText))
+        .then((response) => response.json())
+        .then((json) => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
+
+btnReset.addEventListener("click", () => {
+    input.value = "";
+    output.value = "";
+});
